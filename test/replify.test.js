e94243a05670d873ca91d9803b33bd0ff10709b2
Kit Cambridge <github@kitcambridge.be>
2013-08-13 18:13:01 -0700

    Test establishing and closing a connection.

--- test/replify.test.js
+++ test/replify.test.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
   , http = require('http')
+  , net = require('net')
   , replify = require('../')
   , tap = require('tap')
   , test = tap.test
@@ -29,10 +30,14 @@ test('replify', function (t) {
     // need to give it a couple ticks to setup
     setTimeout(function () {
       t.ok(fs.statSync('/tmp/repl/net-test.sock'), 'repl file exists')
-      t.end()
+
+      var conn = net.connect('/tmp/repl/net-test.sock')
+      conn.resume()
+
+      conn.on('connect', conn.end.bind(conn, '.exit\n'))
+      conn.on('close', t.end.bind(t))
     }, 250)
   })
 
   replify('net-test', app)
-
 })
